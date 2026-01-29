131b4990ed13715fefabde22e9155ee9733ce3ae
Marcos Renann <marcos.renann.br@gmail.com>
2026-01-29 09:15:03 -0300

    feat: button styles (#1)

--- app/src/main/java/com/example/dsnews/MainActivity.kt
+++ app/src/main/java/com/example/dsnews/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dsnews
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,18 +10,62 @@ import com.example.mylibrary.ds.button.DsButton
 import com.example.mylibrary.ds.input.DsInput
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button1 : DsButton
-    private lateinit var input :DsInput
+    private lateinit var input: DsInput
+    private lateinit var btn1: DsButton
+    private lateinit var btn2: DsButton
+    private lateinit var btn3: DsButton
+    private lateinit var btn4: DsButton
+    private lateinit var btn5: DsButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        button1 = findViewById(R.id.btn1)
+
+        input = findViewById(R.id.text)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+
+        setupButtonListeners()
+
+        btn1.apply {
+            setDsBackgroundColorResource(R.color.black)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun setupButtonListeners() {
+        btn1.setDsClickListener {
+            input.setText("Botão Primary clicado!")
+            Toast.makeText(this, "Primary Button", Toast.LENGTH_SHORT).show()
+        }
+
+        btn2.setDsClickListener {
+            input.setText("Botão Secondary clicado!")
+            Toast.makeText(this, "Secondary Button", Toast.LENGTH_SHORT).show()
+        }
+
+        btn3.setDsClickListener {
+            input.setText("Botão Outlined clicado!")
+            Toast.makeText(this, "Outlined Button", Toast.LENGTH_SHORT).show()
+        }
+
+        btn4.setDsClickListener {
+            input.setText("Botão Text clicado!")
+            Toast.makeText(this, "Text Button", Toast.LENGTH_SHORT).show()
+        }
+
+        btn5.setDsClickListener {
+            input.setText("Botão Danger clicado!")
+            Toast.makeText(this, "Danger Button", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/ds/button/Button.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ds/button/Button.kt
@@ -2,37 +2,113 @@ package com.example.mylibrary.ds.button
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import androidx.annotation.ColorInt
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import com.example.mylibrary.R
-import com.example.mylibrary.databinding.DsButtonBinding
-
+import com.google.android.material.button.MaterialButton
 
 class DsButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = com.google.android.material.R.attr.materialButtonStyle
+) : MaterialButton(context, attrs, defStyleAttr) {
 
-    private val binding = DsButtonBinding.inflate(LayoutInflater.from(context), this, true)
+    enum class ButtonType {
+        PRIMARY,
+        SECONDARY,
+        OUTLINED,
+        TEXT,
+        DANGER
+    }
+
+    private var currentType: ButtonType = ButtonType.PRIMARY
 
     init {
-        attrs?.let {
-            val typedArray = context.obtainStyledAttributes(it, R.styleable.DsButton)
-            val textDS = typedArray.getString(R.styleable.DsButton_textDS)
-            binding.button.text = textDS ?: ""
-            typedArray.recycle()
+        insetTop = 0
+        insetBottom = 0
+        textAlignment = TEXT_ALIGNMENT_CENTER
+        gravity = android.view.Gravity.CENTER
+        cornerRadius = resources.getDimensionPixelSize(R.dimen.border_button_radius)
+
+        attrs?.let { attribute ->
+            context.obtainStyledAttributes(attribute, R.styleable.DsButton).apply {
+                val defaultText = text
+                val textDS = getString(R.styleable.DsButton_textDS)
+                text = textDS ?: defaultText
+
+                val typeIndex = getInt(R.styleable.DsButton_buttonType, 0)
+                currentType = ButtonType.entries[typeIndex]
+
+                val bgColor = getColor(R.styleable.DsButton_backgroundColorDS, -1)
+                val txtColor = getColor(R.styleable.DsButton_textColorDS, -1)
+
+                val iconStartRes = getResourceId(R.styleable.DsButton_iconStart, -1)
+                val iconEndRes = getResourceId(R.styleable.DsButton_iconEnd, -1)
+
+                recycle()
+
+                applyButtonType(currentType)
+
+                if (bgColor != -1) setDsBackgroundColor(bgColor)
+                if (txtColor != -1) setTextColor(txtColor)
+                if (iconStartRes != -1) setIconResource(iconStartRes)
+                if (iconEndRes != -1) icon = ContextCompat.getDrawable(context, iconEndRes)
+            }
         }
     }
 
+    fun setButtonType(type: ButtonType) {
+        currentType = type
+        applyButtonType(type)
+    }
+
+    private fun applyButtonType(type: ButtonType) {
+        when (type) {
+            ButtonType.PRIMARY -> {
+                backgroundTintList = ContextCompat.getColorStateList(context, android.R.color.holo_blue_dark)
+                setTextColor(ContextCompat.getColor(context, android.R.color.white))
+                strokeWidth = 0
+            }
+            ButtonType.SECONDARY -> {
+                backgroundTintList = ContextCompat.getColorStateList(context, android.R.color.darker_gray)
+                setTextColor(ContextCompat.getColor(context, android.R.color.white))
+                strokeWidth = 0
+            }
+            ButtonType.OUTLINED -> {
+                backgroundTintList = ContextCompat.getColorStateList(context, android.R.color.transparent)
+                setTextColor(ContextCompat.getColor(context, android.R.color.holo_blue_dark))
+                strokeWidth = 4
+                strokeColor = ContextCompat.getColorStateList(context, android.R.color.holo_blue_dark)
+            }
+            ButtonType.TEXT -> {
+                backgroundTintList = ContextCompat.getColorStateList(context, android.R.color.transparent)
+                setTextColor(ContextCompat.getColor(context, android.R.color.holo_blue_dark))
+                strokeWidth = 0
+                elevation = 0f
+            }
+            ButtonType.DANGER -> {
+                backgroundTintList = ContextCompat.getColorStateList(context, android.R.color.holo_red_dark)
+                setTextColor(ContextCompat.getColor(context, android.R.color.white))
+                strokeWidth = 0
+            }
+        }
+    }
+
+
     fun setDsBackgroundColor(@ColorInt color: Int) {
-        binding.root.setBackgroundColor(color)
+        backgroundTintList = android.content.res.ColorStateList.valueOf(color)
     }
+
+    fun setDsBackgroundColorResource(@ColorRes colorRes: Int) {
+        backgroundTintList = ContextCompat.getColorStateList(context, colorRes)
+    }
+
     fun setDsText(text: String) {
-        binding.button.text = text
+        this.text = text
     }
+
     fun setDsClickListener(clickListener: () -> Unit) {
-        binding.button.setOnClickListener { clickListener() }
+        setOnClickListener { clickListener() }
     }
 }
